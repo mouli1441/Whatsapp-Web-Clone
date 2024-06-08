06b709a18d0e56de3833eb851a0c034b6e6b96ad
Moulisree Karmakar <moulisreek1441@gmail.com>
2024-06-08 17:45:27 +0530

    Optimize build

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import reducer , {initialState} from "./reducer";
 import { StateProvider } from './StateProvider';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <StateProvider initialState ={initialState} reducer={reducer}>
-    <App />
-    </StateProvider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<StateProvider initialState ={initialState} reducer={reducer}><App /></StateProvider>, rootElement);
+// root.render(
+//   <React.StrictMode>
+//     <StateProvider initialState ={initialState} reducer={reducer}>
+//     <App />
+//     </StateProvider>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
